b621cd6346902c62770d8561ce731e5240a0b330
Damian Rzeszot <damian.rzeszot@gmail.com>
2019-12-28 22:47:54 +0100

    Test

--- config/database.cr
+++ config/database.cr
@@ -4,6 +4,8 @@ AppDatabase.configure do |settings|
   if Lucky::Env.production?
     settings.url = ENV.fetch("DATABASE_URL")
   else
+    puts ENV.inspect
+
     settings.url = ENV["DATABASE_URL"]? || Avram::PostgresURL.build(
       database: database_name,
       hostname: ENV["POSTGRES_HOST"]? || "localhost",
