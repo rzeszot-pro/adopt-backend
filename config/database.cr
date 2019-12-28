e3590bbf2925756d42dca2e595d21f1b18dceac9
Damian Rzeszot <damian.rzeszot@gmail.com>
2019-12-29 00:49:27 +0100

    Update workflow

--- config/database.cr
+++ config/database.cr
@@ -8,7 +8,7 @@ AppDatabase.configure do |settings|
 
     settings.url = ENV["DATABASE_URL"]? || Avram::PostgresURL.build(
       database: database_name,
-      hostname: ENV["POSTGRES_HOST"]? || "postgres",
+      hostname: ENV["POSTGRES_HOST"]? || "localhost",
       username: ENV["POSTGRES_USER"]? || "postgres",
       password: ENV["POSTGRES_PASSWORD"]? || "postgres",
       port: ENV["POSTGRES_PORT"]? || "5432"
