df00469c33f3dfa1ca425a8bef5c8af86aa09a3c
Damian Rzeszot <damian.rzeszot@gmail.com>
2019-12-29 01:42:50 +0100

    Test database url

--- config/database.cr
+++ config/database.cr
@@ -1,26 +1,10 @@
-database_name = "adopt_#{Lucky::Env.name}"
-
 AppDatabase.configure do |settings|
-  if Lucky::Env.production?
-    settings.url = ENV.fetch("DATABASE_URL")
-  else
-    settings.url = ENV["DATABASE_URL"]? || Avram::PostgresURL.build(
-      database: database_name,
-      hostname: ENV["POSTGRES_HOST"]? || "localhost",
-      username: ENV["POSTGRES_USER"]? || "postgres",
-      password: ENV["POSTGRES_PASSWORD"]? || "postgres",
-      port: ENV["POSTGRES_PORT"]? || "5432"
-    )
-  end
+  settings.url = ENV.fetch("DATABASE_URL")
 end
 
 Avram.configure do |settings|
   settings.database_to_migrate = AppDatabase
-
-  # In production, allow lazy loading (N+1).
-  # In development and test, raise an error if you forget to preload associations
   settings.lazy_load_enabled = Lucky::Env.production?
 
-  # Uncomment the next line to log all SQL queries
-  # settings.query_log_level = ::Logger::Severity::DEBUG
+  settings.query_log_level = ::Logger::Severity::DEBUG unless Lucky::Env.production?
 end
